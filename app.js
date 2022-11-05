f8bdbf7afb3747f6b9e65d9eac267ff6dfa716c6
MuhammadAliShoaib <malishoaib.2001@gmail.com>
2022-11-05 07:11:48 +0500

    Project Started

---
+++ app.js
@@ -0,0 +1,40 @@
+// alert('Alert message popped');
+// window.alert('Alert message popped');
+
+// var abc = "Welcome to Javascript";
+//overwrites the variable
+// var abc = "Hello";
+// alert(abc);
+
+// var a = 10;
+// console.log(a);
+
+// var a = 10;
+// var b = 20;
+// var c = (a+b)*a;
+// console.log(c);
+
+// var a;
+// a="abc";
+// console.log(a);
+
+// var a =123;
+// a="abc";
+// console.log(a);
+
+// var a=20;
+// var b="10";
+// var c = a-b;
+// prints 10 bcz string also consists of a number
+// if number not present for e.g "10a" so NaN
+// but if + operator used btw a and b so it concatenates 
+// console.log(c);
+
+// var a = 10;
+// a++;
+// console.log(a);
+
+// var a =10;
+// var b = a++;
+// console.log(b)
+
